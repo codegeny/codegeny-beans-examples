656f2955a93114335e49651c08988dcc807887fb
Xavier Dury <xavier@codegeny.org>
2019-07-31 15:30:50 +0200

    Added readme and license

--- codegeny-beans-examples-jsf/src/main/java/org/codegeny/beans/example/Address.java
+++ codegeny-beans-examples-jsf/src/main/java/org/codegeny/beans/example/Address.java
@@ -1,8 +1,8 @@
 /*-
  * #%L
- * codegeny-beans
+ * codegeny-beans-examples
  * %%
- * Copyright (C) 2016 - 2018 Codegeny
+ * Copyright (C) 2016 - 2019 Codegeny
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
